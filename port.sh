3b2749881bffacc5b105bb46405c965fc51824d2
hefeng <hffhhffh3@gmail.com>
2026-07-24 08:53:36 +0800

    Update port.sh

--- port.sh
+++ port.sh
@@ -540,15 +540,15 @@ else
     blue "File $targetVintf not found."
 fi
 
-if [[ ${port_rom_code} == "sheng" ]] || [[ ${port_android_version} == "15" ]];then
-    blue "Skip StrongToast UI fix"
-elif [[ ${port_rom_code} == "houji" ]] || [[ ${port_rom_code} == "shennong" ]] ;then
-    blue "左侧挖孔灵动岛修复" "StrongToast UI fix"
-    patch_smali "MiuiSystemUI.apk" "MIUIStrongToast\$2.smali" "const\/4 v7\, 0x0" "iget-object v7\, v1\, Lcom\/android\/systemui\/toast\/MIUIStrongToast;->mRLLeft:Landroid\/widget\/RelativeLayout;\\n\\tinvoke-virtual {v7}, Landroid\/widget\/RelativeLayout;->getLeft()I\\n\\tmove-result v7\\n\\tint-to-float v7,v7"
-else
-blue "左侧挖孔灵动岛修复" "StrongToast UI fix"
-    patch_smali "MiuiSystemUI.apk" "MIUIStrongToast\$2.smali" "const\/4 v9\, 0x0" "iget-object v9\, v1\, Lcom\/android\/systemui\/toast\/MIUIStrongToast;->mRLLeft:Landroid\/widget\/RelativeLayout;\\n\\tinvoke-virtual {v9}, Landroid\/widget\/RelativeLayout;->getLeft()I\\n\\tmove-result v9\\n\\tint-to-float v9,v9"
-fi
+# if [[ ${port_rom_code} == "sheng" ]] || [[ ${port_android_version} == "15" ]];then
+#     blue "Skip StrongToast UI fix"
+# elif [[ ${port_rom_code} == "houji" ]] || [[ ${port_rom_code} == "shennong" ]] ;then
+#     blue "左侧挖孔灵动岛修复" "StrongToast UI fix"
+#     patch_smali "MiuiSystemUI.apk" "MIUIStrongToast\$2.smali" "const\/4 v7\, 0x0" "iget-object v7\, v1\, Lcom\/android\/systemui\/toast\/MIUIStrongToast;->mRLLeft:Landroid\/widget\/RelativeLayout;\\n\\tinvoke-virtual {v7}, Landroid\/widget\/RelativeLayout;->getLeft()I\\n\\tmove-result v7\\n\\tint-to-float v7,v7"
+# else
+#     blue "左侧挖孔灵动岛修复" "StrongToast UI fix"
+#     patch_smali "MiuiSystemUI.apk" "MIUIStrongToast\$2.smali" "const\/4 v9\, 0x0" "iget-object v9\, v1\, Lcom\/android\/systemui\/toast\/MIUIStrongToast;->mRLLeft:Landroid\/widget\/RelativeLayout;\\n\\tinvoke-virtual {v9}, Landroid\/widget\/RelativeLayout;->getLeft()I\\n\\tmove-result v9\\n\\tint-to-float v9,v9"
+# fi
 
 if [[ ${is_eu_rom} == "true" ]];then
     patch_smali "miui-services.jar" "SystemServerImpl.smali" ".method public constructor <init>()V/,/.end method" ".method public constructor <init>()V\n\t.registers 1\n\tinvoke-direct {p0}, Lcom\/android\/server\/SystemServerStub;-><init>()V\n\n\treturn-void\n.end method" "regex"
@@ -812,7 +812,9 @@ if [[ ${base_rom_code} == "alioth" ]];then
     unlock_device_feature "whether support dc backlight " "bool" "support_dc_backlight"
     unlock_device_feature "whether backlight bit switch " "bool" "support_backlight_bit_switch"
 fi
+# TODO 不知道是否要换
 patch_smali "PowerKeeper.apk" "DisplayFrameSetting.smali" "unicorn" "umi"
+
 if [[ ${is_eu_rom} == true ]];then
     patch_smali "MiSettings.apk" "NewRefreshRateFragment.smali" "const-string v1, \"btn_preferce_category\"" "const-string v1, \"btn_preferce_category\"\n\n\tconst\/16 p1, 0x1"
 
